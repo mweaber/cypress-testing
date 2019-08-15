54904ee8bc17c45e7e5827496d8d464e1d806d1a
Matthew Weaber <matthewweaber89@gmail.com>
2019-08-15 19:18:53 -0400

    Set up two test, clicking links on DOM, adding text to fields

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,57 @@
+// ------- Passing Test
+// describe('My First Test', function() {
+//     it('Does not do much!', function() {
+//       expect(true).to.equal(true)
+//     })
+//   })
+
+// ------- Failing Test
+//   describe('My First Test', function() {
+//     it('Does not do much!', function() {
+//       expect(true).to.equal(false)
+//     })
+//   })
+
+// ------- Writing Test to visit URL
+// describe('My First Test', function() {
+//     it('Visits the Kitchen Sink', function() {
+//       cy.visit('https://example.cypress.io')
+//     })
+//   })
+
+// ------- First Assertion
+// (Includes Visit, Contains, Click, URL, Should, Get, Type)
 describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(true)
+    it('Visits the Kitchen Sink', function() {
+        cy.visit('http://example.cypress.io')
+
+        cy.contains('type').click()
+
+        cy.url().should('include', 'commands/actions')
+
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
     })
-  })
+})
+
+describe('My Second Test', () => {
+    it('Clicks a different link', () => {
+        cy.visit('http://example.cypress.io')
+        cy.contains('scrollTo').click()
+        cy.url().should('include', 'commands/actions')
+        cy.get('#description')
+            .type('Adding text to this field')
+            .should('have.value', 'Adding text to this field')
+
+        cy.get('#couponCode1')
+            .type('Promo Code')
+            .should('have.value', 'Promo Code')
+
+        cy.get('.action-form > .btn').click()
+        
+        cy.get('.well > p')
+            
+    
+    })
+})
